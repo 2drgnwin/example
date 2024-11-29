b974e4f08e98b953b629118d9de49a3738c3f45c
2drgnwin <80686856+2drgnwin@users.noreply.github.com>
2024-11-29 18:24:48 +0900

    Add files via upload

--- 6. 제2유형-실전문제.r
+++ 6. 제2유형-실전문제.r
@@ -8,37 +8,70 @@ train = read.csv("data/customer_train.csv")
 test = read.csv("data/customer_test.csv")
 
 # 사용자 코딩
-library(dplyr); library(caret); library(randomForest); library(ModelMetrics)
-## data processing
-#summary(train)
-#str(train)
-train$환불금액 <- ifelse(is.na(train$환불금액), 0, train$환불금액)
-train$성별 <- as.factor(train$성별)
-#str(train)
-## model construction
-#idx <- sample(1:nrow(train), nrow(train)*0.8)
-#train_1 <- train[idx,]
-#train_2 <- train[-idx,]
 
-#md <- randomForest(성별~., data = train_1[,-1], ntree=400, mtry=3, do.trace=TRUE, proximity=TRUE)
-#md
+# library
+library(dplyr)
+library(caret)
+library(randomForest)
+library(ModelMetrics)
+#ls("package:caret")
 
-#pred <- predict(md, newdata = train_2[,-1])
+# 1. data
+###############
+ #print(train %>% str)
+ #print(train %>% summary)
 
-#auc(train_2$성별,pred)
 
-## model construction for test
+# 2. D.P
+###############
+#df_train <- train[,-1]
+#df_train$성별<- as.factor(df_train$성별) 
+#df_train$주구매상품<- as.factor(df_train$주구매상품)
+#df_train$주구매지점<- as.factor(df_train$주구매지점)
+# df_train$환불금액 <- ifelse(is.na(df_train$환불금액), 0, df_train$환불금액)
 
-test$환불금액 <- ifelse(is.na(test$환불금액), 0, test$환불금액)
-#test$성별 <- as.factor(test$성별)
-str(test)
-md <- randomForest(성별~., data = train[,-1], ntree=100,mtry=3,do.trace=TRUE, proximity =TRUE)
-pred <- predict(md, newdata = test[,-1])
-pred
-##출력
-write.csv(pred,"result.csv",row.names = FALSE)
-a <- read.csv("result.csv")
-a
+
+# 3. split set
+###############
+#set.seed(63)
+#partition<- createDataPartition(df$성별, p=0.7, list=FALSE)
+#train_1 <- df[partition,]
+#train_2 <- df[-partition,]
+#print(nrow(train_1));print(nrow(train_2))
+#table(train_1$성별)
+
+
+# 4. train1 - M.C
+#################
+#md <- randomForest(성별~., data = train_1, ntree=400)
+#print(md)
+
+# 5. train1 M.E
+####################
+#pred <- predict(md, newdata = train_2)
+#print(caret::confusionMatrix(pred, reference = train_2$성별))
+#print(auc(train_2$성별,pred))
+
+# 6. Final prediction
+#train <- train %>% filter(회원ID != 1659)
+df_train <- train[,-1]
+df_train$성별<- as.factor(df_train$성별) 
+df_train$주구매상품<- as.factor(df_train$주구매상품)
+df_train$주구매지점<- as.factor(df_train$주구매지점)
+df_train$환불금액 <- ifelse(is.na(df_train$환불금액), 0, df_train$환불금액)
+
+df_test <- test[,-1]
+df_test$환불금액 <- ifelse(is.na(df_test$환불금액), 0, df_test$환불금액)
+df_test$주구매상품<- as.factor(df_test$주구매상품)
+df_test$주구매지점<- as.factor(df_test$주구매지점)
+
+levels(df_test$'주구매상품')<-levels(df_train$'주구매상품') 
+
+md <- randomForest(성별~., data = df_train, ntree=400)
+pred <- predict(md, newdata = df_test)
+
+table(pred)
+# 
 # 답안 제출 참고
 # 아래 코드는 예시이며 변수명 등 개인별로 변경하여 활용
 # write.csv(data.frame변수,"result.csv",row.names = FALSE)
